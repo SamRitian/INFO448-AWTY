39d1f9d9962877af49134098c4c7ce33efcb3483
SamRitian <xyou@uw.edu>
2024-02-15 22:53:52 -0800

    completed

---
+++ app/src/main/java/edu/uw/ischool/xyou/awty/AWTYService.kt
@@ -0,0 +1,67 @@
+package edu.uw.ischool.xyou.awty
+
+import android.app.IntentService
+import android.content.Intent
+import android.os.Handler
+import android.util.Log
+import android.widget.Toast
+
+/**
+ * An [IntentService] subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ */
+class AWTYService : IntentService("AWTYService") {
+
+    private val TAG = "AWTYService"
+
+    private lateinit var mHandler: Handler
+    private var isRunning = false
+    private var phoneNumber = ""
+    private var message = ""
+    private var time = 0
+
+    override fun onCreate() {
+        super.onCreate()
+        mHandler = Handler()
+
+        isRunning = true
+
+        Log.i(TAG, "Service created")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // get intent data
+        phoneNumber = reformatPhoneNumber(intent?.getStringExtra("phone_number")!!)
+        message = intent.getStringExtra("message")!!
+        time = intent.getStringExtra("time")!!.toInt()
+
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        while (isRunning) {
+            Log.i(TAG, "Sending message to $phoneNumber: $message")
+
+            mHandler.post {
+                Toast.makeText(this, "$phoneNumber: $message", Toast.LENGTH_SHORT).show()
+            }
+
+            try {
+                Thread.sleep((time * 1000).toLong())
+            } catch (e: InterruptedException) {
+                Thread.currentThread().interrupt()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        Log.i(TAG, "Service destroyed")
+        isRunning = false
+        super.onDestroy()
+    }
+
+    // takes in a string of length 10, returns (xxx) xxx-xxxx
+    private fun reformatPhoneNumber(phoneNumber: String): String {
+        return "(${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3, 6)}-${phoneNumber.substring(6, 10)}"
+    }
+}
--- app/src/main/java/edu/uw/ischool/xyou/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/xyou/awty/MainActivity.kt
@@ -1,11 +1,59 @@
 package edu.uw.ischool.xyou.awty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RelativeLayout
+import android.widget.Toast
+import kotlin.reflect.typeOf
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+    private var started = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set up the start/stop button
+        val startStopButton = findViewById<Button>(R.id.start_stop_button)
+        if (started) startStopButton.text = getString(R.string.stop_button)
+        else startStopButton.text = getString(R.string.start_button)
+
+        startStopButton.setOnClickListener {
+            // get user input
+            val phoneNumber = findViewById<EditText>(R.id.phone_number_input).text.toString()
+            val message = findViewById<EditText>(R.id.message_input).text.toString()
+            val time = findViewById<EditText>(R.id.time_input).text.toString()
+
+            if (isValidInput(phoneNumber, message, time)) {
+                Log.i(TAG, "In this branch")
+                started = !started
+                if (started) {
+                    // Start the service
+                    startStopButton.text = getString(R.string.stop_button)
+                    val intent = Intent(this, AWTYService::class.java).apply {
+                        putExtra("phone_number", phoneNumber.toString())
+                        putExtra("message", message.toString())
+                        putExtra("time", time.toString())
+                    }
+                    startService(intent)
+                } else {
+                    // Stop the service
+                    startStopButton.text = getString(R.string.start_button)
+                    val intent = Intent(this, AWTYService::class.java)
+                    stopService(intent)
+                }
+            } else {
+                Toast.makeText(this, "Invalid input", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isValidInput(phoneNumber: String, message: String, time: String): Boolean {
+        return phoneNumber.length == 10 && message != "" && time != ""
     }
 }
